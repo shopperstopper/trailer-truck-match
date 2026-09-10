6910dd04a2c6ab32020c8cb5835a1d1664d683a7
shopperstopper <shopperstopper@gmail.com>
2026-09-09 20:52:01 -0700

    Update Trailer-Vehicle-Match.py

--- Trailer-Vehicle-Match.py
+++ Trailer-Vehicle-Match.py
@@ -95,7 +95,7 @@ dealer_info = DEALER_ACCOUNTS[dealer_key]
 
 st.set_page_config(
     page_title=f"{dealer_info['name']} | Tow Match Pro",
-    page_icon=dealer_info["rv-icon.png"],
+    page_icon="rv-icon.png",
     layout="centered",
     initial_sidebar_state="collapsed",
 )
